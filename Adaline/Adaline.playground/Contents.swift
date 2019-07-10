84ec927e4bbe281dd6a7efc32476b31987dfc8bc
Jully Nobre <jullynobre1@gmail.com>
2019-07-10 11:25:39 -0300

    Add Adaline

--- Adaline/Adaline.playground/Contents.swift
+++ Adaline/Adaline.playground/Contents.swift
@@ -1,3 +1,66 @@
 import Cocoa
 
-var str = "Hello, playground"
+// y = ax - b
+// w0 = -b w1 = a
+// y = w1x + w0
+
+// x: value, y: label
+struct DataObject {
+    let x: Double
+    let y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
+
+//return w1x + w0
+func predict(input: Double, weights: [Double]) -> Double {
+    let y = weights[1] * input - weights[0]
+    return y
+}
+
+//Returns trained WEIGHTS (a and b)
+func train(dataArr: [DataObject], learningRate: Double, epochsNumber: Int) -> [Double]{
+    var weights = [Double.random(in: -1.0...1.0),
+                   Double.random(in: -1.0...1.0)]
+    
+    var epochs = 0
+    
+    for _ in 0...epochsNumber {
+        var errors = 0
+        
+        for data in dataArr {
+            let y = predict(input: data.x, weights: weights)
+            if y != data.y {
+                errors += 1
+                
+                let error = data.y - y
+
+                weights[0] = weights[0] + learningRate * error * -1
+                weights[1] = weights[1] + learningRate * error * data.x
+            }
+        }
+        epochs += 1
+    }
+    
+    return weights
+}
+
+// y = x + 5
+let dataArr = [DataObject(x: -3, y: 2),
+               DataObject(x: -2, y: 3),
+               DataObject(x: -1, y: 4),
+               DataObject(x: 0, y: 5),
+               DataObject(x: 1, y: 6),
+               DataObject(x: 2, y: 7),
+               DataObject(x: 3, y: 8),
+]
+
+let weights = train(dataArr: dataArr, learningRate: 0.1, epochsNumber: 20)
+
+predict(input: 4, weights: weights)
+predict(input: 5, weights: weights)
+predict(input: -15, weights: weights)
+
